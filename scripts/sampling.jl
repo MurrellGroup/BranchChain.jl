ce7dba5ae45e2192ba31f6ee9d87be7150065463
murrellb <murrellb@gmail.com>
2025-11-29 22:25:32 +0100

    Ack

--- scripts/sampling.jl
+++ scripts/sampling.jl
@@ -15,6 +15,8 @@ pdb_path = "9IQP"
 to_mask = ["VARGVGVYGMHWFCGEYNFA"] #<- The CDR3
 
 prot = BranchChain.pdbentry(pdb_path)[2:2]; #NOTE <- only taking the second chain in this case.
-samp = design(model, X1_from_pdb(prot, to_mask), path = "test.pdb", vidpath = "test_vid") #steps = Float32.([0:0.1:0.9; 0.925:0.025:0.985; 0.99; 0.999; 1.0])
 
-ProtPlot.animate_trajectory_dir(viddir*".mp4", [d for d in readdir(viddir, join=true) if split(d,"/")[end] != ".DS_Store"]|> reverse, color_by=[:chain, :numbering])
+vidpath = "test_vid"
+samp = design(model, X1_from_pdb(prot, to_mask), path = "test.pdb", vidpath = vidpath) #steps = Float32.([0:0.1:0.9; 0.925:0.025:0.985; 0.99; 0.999; 1.0])
+
+ProtPlot.animate_trajectory_dir(vidpath*".mp4", [d for d in readdir(vidpath, join=true) if split(d,"/")[end] != ".DS_Store"]|> reverse, color_by=[:chain, :numbering])
